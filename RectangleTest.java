cc834b897cc55f78c32d2f95b2c852fb127a8d24
Kasra <kasra138079@gmail.com>
2023-11-14 02:08:28 +0330

    Add modifiable dimension tests

--- RectangleTest.java
+++ RectangleTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
 
 public class RectangleTest {
 
@@ -18,5 +19,43 @@ public class RectangleTest {
     @Test(expected = IllegalArgumentException.class)
     public void testAreaWithNegativeNumbers() {
         new Rectangle(-5, 4);
+    }
+    private Rectangle rectangle;
+
+    @Before
+    public void setUp() {
+        rectangle = new Rectangle(5, 4);
+    }
+
+    @Test
+    public void testGetLength() {
+        assertEquals(5, rectangle.getLength(), 0.001);
+    }
+
+    @Test
+    public void testGetWidth() {
+        assertEquals(4, rectangle.getWidth(), 0.001);
+    }
+
+    @Test
+    public void testSetLength() {
+        rectangle.setLength(10);
+        assertEquals(10, rectangle.getLength(), 0.001);
+    }
+
+    @Test
+    public void testSetWidth() {
+        rectangle.setWidth(8);
+        assertEquals(8, rectangle.getWidth(), 0.001);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetNegativeLength() {
+        rectangle.setLength(-3);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetNegativeWidth() {
+        rectangle.setWidth(-3);
     }
 }
