60636138f7f88e896c1403836fca5bf6b0b5b6a8
Kasra <kasra138079@gmail.com>
2023-11-14 02:26:42 +0330

    Add Square tests

--- RectangleTest.java
+++ RectangleTest.java
@@ -59,3 +59,27 @@ public class RectangleTest {
         rectangle.setWidth(-3);
     }
 }
+
+public class SquareTest {
+
+    @Test
+    public void testSquareCreation() {
+        Square square = new Square(5);
+        assertEquals(5, square.getLength(), 0.001);
+        assertEquals(5, square.getWidth(), 0.001);
+    }
+
+    @Test
+    public void testUpdatingLengthUpdatesWidthForSquare() {
+        Square square = new Square(5);
+        square.setLength(10);
+        assertEquals(10, square.getWidth(), 0.001);
+    }
+
+    @Test
+    public void testUpdatingWidthUpdatesLengthForSquare() {
+        Square square = new Square(5);
+        square.setWidth(8);
+        assertEquals(8, square.getLength(), 0.001);
+    }
+}
