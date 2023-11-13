2df5b322d75cd950cc4206dc43c0fb9f735c00ff
Kasra <kasra138079@gmail.com>
2023-11-14 01:43:05 +0330

    Add rectangle class and pass tests

--- Rectangle.java
+++ Rectangle.java
@@ -1,2 +1,16 @@
 public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        if (length < 0 || width < 0) {
+            throw new IllegalArgumentException("Length and width must be non-negative");
+        }
+        this.length = length;
+        this.width = width;
+    }
+
+    public double computeArea() {
+        return length * width;
+    }
 }
