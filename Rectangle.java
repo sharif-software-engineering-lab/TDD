e9936637cb1d0d7435701ccd4d220d60acbed507
Kasra <kasra138079@gmail.com>
2023-11-14 01:45:19 +0330

    Refactor Rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -1,13 +1,17 @@
 public class Rectangle {
-    private double length;
-    private double width;
+    private final double length;
+    private final double width;
 
     public Rectangle(double length, double width) {
+        validateDimensions(length, width);
+        this.length = length;
+        this.width = width;
+    }
+
+    private void validateDimensions(double length, double width) {
         if (length < 0 || width < 0) {
             throw new IllegalArgumentException("Length and width must be non-negative");
         }
-        this.length = length;
-        this.width = width;
     }
 
     public double computeArea() {
